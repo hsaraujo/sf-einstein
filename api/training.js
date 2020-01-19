0daba9665ebaa69652f2ad6d8576d4a5235dcdb9
Heitor Araujo <heitor.saraujo@gmail.com>
2020-01-19 22:28:41 +1300

    adds get and perform training

---
+++ api/training.js
@@ -0,0 +1,49 @@
+const fetch = require('node-fetch');
+const options = require('../utils/options');
+const tokenUtils = require('./token-utils');
+const FormData = require('form-data');
+
+async function getStatus(modelId) {
+
+    var token = await tokenUtils.getAccessToken(options.options.accountId, options.options.privateKey);
+
+    const response = await fetch(options.options.baseUrl + '/v2/vision/train/' + modelId, {
+        method: 'GET',
+        headers: {
+            'Authorization' : 'Bearer ' + token
+        }
+        
+    });
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+
+    return response.json();
+}
+
+async function trainDataset(name, datasetId) {
+
+    var token = await tokenUtils.getAccessToken(options.options.accountId, options.options.privateKey);
+
+    const form = new FormData();
+    form.append('name', name);
+    form.append('datasetId', datasetId)
+
+    const response = await fetch(options.options.baseUrl + '/v2/vision/train/', {
+        method: 'POST',
+        headers: {
+            'Authorization' : 'Bearer ' + token
+        },
+        body: form
+        
+    });
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+
+    return response.json();
+}
+
+module.exports.getStatus = getStatus;
+module.exports.trainDataset = trainDataset;
+
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const tokenUtils = require('./api/token-utils');
 const options = require('./utils/options');
 const datasets = require('./api/datasets');
+const training = require('./api/training');
 
 function getToken(){
 
@@ -15,7 +16,13 @@ module.exports.setup = (options) => {
 };
 module.exports.getDatasets = async () => {
     return await datasets.getAll();
-}
+};
 module.exports.getDataset = async (datasetId) => {
     return await datasets.get(datasetId);
+};
+module.exports.getTrainingStatus = async (modelId) => {
+    return await training.getStatus(modelId);
+};
+module.exports.trainDataSet = async (name, datasetId) => {
+    return await training.trainDataset(name, datasetId);
 }
